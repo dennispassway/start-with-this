80c77eaf87425a0396c019596df77294a32db9c8
Dennis Passway <dennispassway@gmail.com>
2017-07-20 14:02:12 +0200

    Done, figure CSS out yourself.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,26 +4,52 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import thunkMiddleware from 'redux-thunk'
 import { AppContainer } from 'react-hot-loader'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import Immutable from 'immutable'
 
-import App from './app'
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
+import setUpSocket from './socket'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { isProd } from '../shared/util'
+
+// eslint-disable-next-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+/* eslint-enable no-underscore-dangle */
+
+const store = createStore(combineReducers(
+  { hello: helloReducer },
+),
+{ hello: Immutable.fromJS(preloadedState.hello) },
+composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
-const wrapApp = AppComponent => (
-  <AppContainer>
-    <AppComponent />
-  </AppContainer>
+const wrapApp = (AppComponent, reduxStore) => (
+  <Provider store={reduxStore}>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
+  </Provider>
 )
 
-ReactDOM.render(wrapApp(App), rootEl)
+ReactDOM.render(wrapApp(App, store), rootEl)
 
 if (module.hot) {
   // flow-disable-next-line
-  module.hot.accept('./app', () => {
+  module.hot.accept('../shared/app', () => {
     // eslint-disable-next-line global-require
-    const NextApp = require('./app').default
-    ReactDOM.render(wrapApp(NextApp), rootEl)
+    const NextApp = require('../shared/app').default
+    ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+setUpSocket(store)
